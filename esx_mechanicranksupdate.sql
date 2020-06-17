1d1d8b79f6dfb894bb2d1953cd2124f337f93d30
Aljon Santiago <santiagoaj2001@gmail.com>
2020-06-18 00:25:26 +0800

    Updated Mechanic Job Rank Names and Salary

--- esx_mechanicranksupdate.sql
+++ esx_mechanicranksupdate.sql
@@ -4,11 +4,25 @@ UPDATE `job_grades`
 SET label = 
     case grade
     when 0 then 'Trainee'
-    when 1 then 'Junior Mechanic'
-    when 2 then 'Senior Mechanic'
-    when 3 then 'Mechanic Supervisor'
-    when 4 then 'Head Mechanic'
+    when 1 then 'Junior'
+    when 2 then 'Senior'
+    when 3 then 'Supervisor'
+    when 4 then 'Head Mech'
     else ''
+    end
+    where grade IN (
+        0, 1, 2, 3, 4
+    );
+
+UPDATE `job_grades`
+SET salary = 
+    case grade
+    when 0 then 500
+    when 1 then 1000
+    when 2 then 1500
+    when 3 then 2000
+    when 4 then 2500
+    else 0
     end
     where grade IN (
         0, 1, 2, 3, 4
